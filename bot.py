f8411e695e8133347a21502e2d86a2d777a3e186
aierw <125786895+aierw@users.noreply.github.com>
2023-08-21 22:07:23 -0700

    1.03
    Added table to /start comand, can receive info about ETH and BTC.

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 import requests
 from datetime import datetime
 import telebot
-from auth_token import token
+from auth_token1 import token
 from comands import telegram_bot
 
+#BTC
 def get_data():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
     response = req.json()
     sell_price = response["btc_usd"]["sell"]
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%H')}\nSell BTC price: {sell_price}")
+
+#ETH
+def get_data():
+    req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+    response = req.json()
+    sell_price = response["eth_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%H')}\nSell ETH price: {sell_price}")
     
 
 if __name__ == '__main__':
--- comands.py
+++ comands.py
@@ -3,14 +3,60 @@ import requests
 from datetime import datetime
 from auth_token import token
 
+menu_options = [
+    "ETH Price",
+    "BTC Price",
+    "Option 3"
+]
+
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
 
     @bot.message_handler(commands=["start"])
-    def start_message(message):
-        bot.send_message(message.chat.id,"Добро пожаловать ! Это Украинский телеграм бот крипто оповещений 223 ")
+    def handle_start(message):
+        markup = telebot.types.InlineKeyboardMarkup(row_width=2)
 
+        for option in menu_options:
+            button = telebot.types.InlineKeyboardButton(option, callback_data=option)
+            markup.add(button)
+
+        bot.send_message(message.from_user.id, "Welcome to Trading Alarm bot - useful tool for every trader. Choose from the followign comands:", reply_markup=markup)
+        
+    #запрос ETH
+    @bot.callback_query_handler(func=lambda call: True)
+    def handle_button_click(call):
+        option = call.data
+        
+        if option == "ETH Price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+                response = req.json()
+                sell_price = response["eth_usd"]["sell"]
+                bot.send_message(call.from_user.id,f"{datetime.now().strftime('%Y-%m-%d %H:%H')}\nSell ETH price: {sell_price}")
+         
+            except Exception as ex:
+                print(ex)
+                bot.send_message(call.messege.chat.id,"Can not collect information, try again later")
+
+        else:
+            bot.send_message(call.from_user.id, "wrong comand")
+            
+        if option == "BTC Price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response["btc_usd"]["sell"]
+                bot.send_message(call.from_user.id,f"{datetime.now().strftime('%Y-%m-%d %H:%H')}\nSell BTC price: {sell_price}")
+         
+            except Exception as ex:
+                print(ex)
+                bot.send_message(call.from_user.id,"Can not collect information, try again later")
+
+        else:
+            bot.send_message(call.messege.chat.id, "wrong comand")
     
+
+    #начало других команд
     @bot.message_handler(commands=["help"])
     def start_message(message):
         bot.send_message(message.chat.id,"Help with your problem")
@@ -26,7 +72,7 @@ def telegram_bot(token):
          
             except Exception as ex:
                 print(ex)
-                bot.send_message(message.chat.id,"Damn.....")
+                bot.send_message(message.chat.id,"Can not collect information, try again later")
 
         else:
             bot.send_message(message.chat.id, "wrong comand")
